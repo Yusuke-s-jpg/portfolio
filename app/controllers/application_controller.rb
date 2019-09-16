3f9befeca3da193dbd06a8dcfd5ec2fab86d7e95
Yusuke Sato <yusuke_s@YusukenoMacBook-ea.local>
2019-09-16 09:49:13 +0900

    modify index.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def authenticate_user
    if @current_user == nil
     flash[:notice] = "You need to login"
-          redirect_to("/login")
+          redirect_to("users/login")
    end
   end
 
